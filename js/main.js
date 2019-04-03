823bdd0e95cdfa44d01b2915d13cac8dc42c702a
wizhi.dev <wzhi.dev@gmail.com>
2019-04-04 06:22:32 +0800

    Site updated: 2019-04-04 06:22:32

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-new Vue({
-	el: '#replica-app',
-	data: {
-		moment: moment
-	}
-});
+/**
+ * prevent default form submit, submit query to google custom search
+ * @returns {boolean}
+ */
+function search() {
+  var $gsc = document.getElementById('gsc-i-id1');
+  if ($gsc) {
+    $gsc.value = document.getElementById('query').value;
+    document.getElementsByClassName('gsc-search-button')[1].click();
+  }
+  return false;
+}
+
+document.getElementsByClassName('clear-search')[0].onclick = function() {
+  document.getElementById('query').value = '';
+};
